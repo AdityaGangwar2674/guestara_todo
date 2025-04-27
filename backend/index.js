57254fe8a3f79eab07a4c59b67fc2cf3c2a703d5
Aditya Gangwar <manojkumar1826@rediffmail.com>
2025-04-28 00:00:40 +0530

    outline done

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
