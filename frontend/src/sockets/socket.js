96d20e9db4d59c856ee89786f2f97130a382d0a4
Aditya Gangwar <manojkumar1826@rediffmail.com>
2025-07-15 19:56:32 +0530

    deploy

--- frontend/src/sockets/socket.js
+++ frontend/src/sockets/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BASE_URL, {
+export const socket = io(process.env.REACT_APP_BASE_URL, {
   withCredentials: true,
 });
 
